a1f3b8277304a51db109b73c1bb49372668fbe65
leofrio <leogaspar01@hotmail.com>
2026-02-04 15:01:15 -0300

    starting out angular project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes) ,
+    providePrimeNG()
   ]
 };
