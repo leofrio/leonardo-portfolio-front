a21c5b4c043cdef350415fb4338a063b329f9aad
leofrio <leogaspar01@hotmail.com>
2026-03-05 16:53:25 -0300

    animations a

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AnimateOnScrollModule, } from 'primeng/animateonscroll'; 
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AnimateOnScrollModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
