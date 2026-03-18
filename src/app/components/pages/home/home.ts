e10120e951f30134b08d8b97739f595a463400e2
leofrio <leogaspar01@hotmail.com>
2026-03-18 16:06:29 -0300

    adding quote text and its animations

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { fontaIcons } from '../../../static/fontaIcons';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  readonly fontaIcons = fontaIcons;
   readonly fullName = ['Leonardo', 'Pontes', 'Gaspar'];
+  scrollIntoView (selector:string) {  
+  const scrollToDocument = document.querySelector(selector) as HTMLElement ; 
+  scrollToDocument.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+  }
 }
